2ccb2c890805338e805832124ae7bfa82762338d
Vaibhav M N <149036369+Vaibhav2154@users.noreply.github.com>
2024-03-03 13:50:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,35 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+
+//Task 11 Understand BCrypt Hashes
+const bcrypt = require('bcrypt');
+
 fccTesting(app);
+
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
+//Task 13 Hash and Compare Passwords Asynchronously
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+//Hash and Compare Passwords Asynchronously
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
 
 
 //END_ASYNC
 
+//Task 14 Hash and Compare Passwords Synchronously
 //START_SYNC
-
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 //END_SYNC
